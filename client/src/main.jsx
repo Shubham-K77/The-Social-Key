2dc60e9dfe3602b1faf3b1e24f3e433c398c2ee9
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2025-02-12 19:50:54 +0545

    Removing Strict Mode!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,16 +8,13 @@ import { BrowserRouter } from "react-router-dom";
 import { SocketContextProvider } from "../context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  //StrictMode renders every component twice! On Development Mode!
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <SnackbarProvider>
-          <SocketContextProvider>
-            <App />
-          </SocketContextProvider>
-        </SnackbarProvider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <SnackbarProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
+      </SnackbarProvider>
+    </BrowserRouter>
+  </Provider>
 );
